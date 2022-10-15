7651a105535b3289413810fd5d6dfb291718e965
Noora <you@example.comnoorahakim14@gmail.com>
2022-10-15 13:22:19 +0300

    assignment completed

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,12 @@
-// Code Keypad Component Here
-
-function Keypad (){
+function Keypad() {
+    const passwordHandler = () => {
+      console.log("Entering password...");
+    };
     return (
-        <div></div>
-    )
-}
-
-export default Keypad;
+      <div>
+        <input type="password" onChange={passwordHandler}></input>
+      </div>
+    );
+  }
+  
+  export default Keypad;
